afa7bc1721b1f75fbd5d3f9609aa7a669ea36509
unknown <350535629@qq.com>
2016-12-08 23:39:15 +0800

    Finished,but I was crawler-countered.

--- second-crawler.py
+++ second-crawler.py
@@ -3,6 +3,7 @@
 from bs4 import BeautifulSoup
 import requests
 import sys
+import time
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -12,19 +13,41 @@ sys.setdefaultencoding('utf-8')
 #noveltext = soup.find_all(style="color: rgb(0, 0, 0); font-size: 14px;")
 #for texts in noveltext:
 #    print(texts)
-i=1
-
-while i<77:
-    url = 'http://www.readnovel.com/novel/341658/%d.html' % i
-    i+=1
-    wb_data = requests.get(url)
-    soup = BeautifulSoup(wb_data.text,'lxml')
-    article_text = soup.select('body > div.readcont > div.readcontcenter > div.bannerleft > div.zhangjie > p')
-
-    target = open('temp.txt','a')
-    #target.write(article_text)
-    for texts in article_text:
-#        print (texts.text)
-        target.write(texts.text)
-        target.write('\n')
-    target.close()
+headers = {
+    "Host": "www.readnovel.com",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0",
+    #Referer:http://www.readnovel.com/novel/341658.html
+}
+k=341648
+while k < 351608:
+    i = 1
+    while i<77:
+        print k
+        url = 'http://www.readnovel.com/novel/{0}/{1}.html'.format(k,i)
+        i+=1
+        print(url)
+        #print('test0')
+        wb_data = requests.get(url, headers=headers)
+        #print('test1')
+        soup = BeautifulSoup(wb_data.text,'lxml')
+        #print('test2')
+        article_text = soup.select('body > div.readcont > div.readcontcenter > div.bannerleft > div.zhangjie > p')
+        #print('testover')
+        if len(article_text) == 0:
+            break
+        print("yeah")
+        headers["Referer"] = url
+        #target.write(article_text)
+        wendao = 'temp%d.txt' % k
+        target = open(wendao,'a')
+        for texts in article_text:
+    #        print (texts.text)
+            target.write(texts.text)
+            target.write('\n')
+        target.close()
+        if i%10 == 0:
+            time.sleep(3)
+    k+=1
+    if k%10 == 0:
+        time.sleep(3)
+print("over")
